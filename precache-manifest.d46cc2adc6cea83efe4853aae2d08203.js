e000d1d5aeb42de741af4c5ae2532dff886507c6
root <saood011@yahoo.com>
2019-11-06 11:11:28 +0100

    Updates

---
+++ precache-manifest.d46cc2adc6cea83efe4853aae2d08203.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ffc600076c9f2b3918831ebc409d88",
+    "url": "/githubcardapp/index.html"
+  },
+  {
+    "revision": "aa6fb8d76bcfbc3a48a2",
+    "url": "/githubcardapp/static/css/main.682e06c2.chunk.css"
+  },
+  {
+    "revision": "9937687e492c65df8cb4",
+    "url": "/githubcardapp/static/js/2.93a876bc.chunk.js"
+  },
+  {
+    "revision": "aa6fb8d76bcfbc3a48a2",
+    "url": "/githubcardapp/static/js/main.e815db8c.chunk.js"
+  },
+  {
+    "revision": "d01c7d803eea39ea934e",
+    "url": "/githubcardapp/static/js/runtime-main.750cc19c.js"
+  }
+]);
